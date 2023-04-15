01df6c1fc541761d322907cb978d6d3b64fc6d31
PRASANT GUPTA <prasant123gupta@gmail.com>
2023-04-15 18:03:19 +0530

    katana_added

---
+++ src/components/Body Components/Katana.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import gasopt from '../../images/gassopt.svg'
+import frtlogo from '../../images/frtlogo.svg'
+
+const Katana = () => {
+  return (
+    <div className='katana'>
+      <div className="k1">
+        <div className="headk1">
+          <div>
+            <span className="headk11">P2P </span><span className="headk12">&nbsp;AuditKatana</span>
+          </div>
+          <div className='prodesc'>
+            Product<br /> Description:
+          </div>
+        </div>
+        <div className="gasopt">
+          <img src={gasopt} alt="gas optimization" className='gasicon'/>
+          <div>
+            <div className="headkatana"><span className="blue"> Gas </span>Optimisation:</div>
+            <div className="desckatana">Our BOT service can save you thousands of dollars in gas fees by making your Smart Contract more gas efficient.</div>
+          </div>
+        </div>
+      </div>
+      <div className="k2">
+        <div className="katanadescription">
+         Our Auditkatana is a tool , designed to help identify 100+ smart contract bugs and gas optimisation issues. It can also provide suggestions for improving the code to make it more secure.<br /><br />
+         <span className="blue"> AuditKatana Key Features:</span><br /><br />
+         Automatic code analysis: The bot could automatically scan the smart contract's code for known vulnerabilities, such as reentrancy attacks or integer overflows.<br /><br />
+         Customizable rule sets: It can allow users to specify custom rule sets or best practices to check for compliance with industry standards.
+        </div>
+        <div className="frt">
+          <img src={frtlogo} alt="fast response time" className='frticon'/>
+          <div>
+            <div className="headkatana">Fast Response <span className="blue"> Time:</span></div>
+            <div className="desckatana desckatanafrt"> Our lightning-fast response time ensures that your report will be delivered within few hours, providing you with peace of mind and allowing you to take advantage of the latest opportunities quickly and effortlessly.</div>
+          </div>
+        </div>
+      </div>
+      <div className="k3"></div>
+    </div>
+  )
+}
+
+export default Katana;
--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Main from './Body Components/Main';
 import Nums from './Body Components/Nums';
+import Katana from './Body Components/Katana';
 
 const Body = () => {
   return (
     <div>
       <Main />
       <Nums />
+      <Katana />
     </div>
   )
 }
