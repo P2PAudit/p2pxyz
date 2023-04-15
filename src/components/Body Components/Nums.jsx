52bbfbfce0b74fda069f82a40679bb02ff41c78e
PRASANT GUPTA <prasant123gupta@gmail.com>
2023-04-15 13:15:56 +0530

    numbers_added

---
+++ src/components/Body Components/Nums.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+const Nums = () => {
+  return (
+    <div className='nums'>
+      <div className="cardnum">
+        <div className="head">
+          $10Bn+
+        </div>
+        <div className="desc">
+          Blockchain<br/> Integrations
+        </div>
+      </div>
+      <div className="cardnum">
+        <div className="head">
+          63
+        </div>
+        <div className="desc">
+          Projects<br /> audited
+        </div>
+      </div>
+      <div className="cardnum">
+        <div className="head">
+          $8Mn+
+        </div>
+        <div className="desc">
+          Amount<br/> protected
+        </div>
+      </div>
+      <div className="cardnum">
+        <div className="head">
+          145+
+        </div>
+        <div className="desc">
+          Critical/high/<br />medium Findings
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Nums;
--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Main from './Body Components/Main';
+import Nums from './Body Components/Nums';
 
 const Body = () => {
   return (
     <div>
       <Main />
+      <Nums />
     </div>
   )
 }
