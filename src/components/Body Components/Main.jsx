b16d954a0e6f00e77a660529a919b3334ce5efdc
PRASANT GUPTA <prasant123gupta@gmail.com>
2023-04-15 12:53:16 +0530

    icon added

--- src/components/Body Components/Main.jsx
+++ src/components/Body Components/Main.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
+import coin from '../../images/coin.svg'
 
 const Main = () => {
   return (
-    <div className='main'>
-      <div className="main1">Maximize security and<br/> minimize costs -</div>
-      <div className="main2">Empowering your decentralized dreams with expert<br /> audits and Web3 Security.</div>
-      <div className="main3">P2PAudit helps Web3 projects meet world leading<br /> independent security experts</div>
+    <div className='mainblock'>
+      <div className='main'>
+        <div className="main1">Maximize security and<br/><span className="pink"> minimize</span> costs -</div>
+        <div className="main2">Empowering your decentralized dreams with expert<br /> audits and Web3 Security.</div>
+        <div className="main3">P2PAudit helps Web3 projects meet <span className="pink2"> world leading<br /> independent security experts </span></div>
+        <div className="main4">
+          <div className="applyaudit">Apply as an Auditor</div>
+          <div className="requestaudit">Request an audit</div>
+        </div>
+      </div>
+      <img src={coin} alt="coin" className='coin' />
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import icon1 from '../images/icon1.svg'
 
 const Navbar = () => {
   return (
     <div className='navbar'>
-      <div><img src="" alt="" /></div>
+      <img src={icon1} alt="icon1" />
       <div className='title'>P2PAUDIT.XYZ</div>
-      <div className="navbtn"><div>Book free <br/> Consultant</div></div>
+      <div className="navbtn"><div>Book free <br/> Consultation</div></div>
       <div className="navflex">
         <div>About Us</div>
         <div>Blog</div>
