5684779ea74a9f32431d863a99d3e22b11708e4d
PRASANT GUPTA <prasant123gupta@gmail.com>
2023-04-19 08:19:48 +0530

    scroll behavior fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,8 +5,8 @@ import {Link} from "react-router-dom"
 const Navbar = () => {
   return (
     <div className='navbar' id='#navbar'>
-      <Link to="/"><img src={icon1} alt="icon1" /></Link>
-      <Link to="/" className='title'>P2PAUDIT.XYZ</Link>
+      <Link onClick={() => { window.scrollTo({ top: 0, left: 0, behavior: 'smooth' }); }} to="/"><img src={icon1} alt="icon1" /></Link>
+      <Link onClick={() => { window.scrollTo({ top: 0, left: 0, behavior: 'smooth' }); }} to="/" className='title'>P2PAUDIT.XYZ</Link>
       <div className="navbtn"><div>Book free <br/> Consultation</div></div>
       <div className="navflex">
         <div>About Us</div>
