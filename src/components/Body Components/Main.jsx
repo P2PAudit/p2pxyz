2e52a9f9f0a745649a71bfb5e383cf999ab11c2a
PRASANT GUPTA <prasant123gupta@gmail.com>
2023-04-26 11:32:37 +0530

    mian mobile update

--- src/components/Body Components/Main.jsx
+++ src/components/Body Components/Main.jsx
@@ -9,11 +9,11 @@ const Main = () => {
         <div className="main1">Maximize security<br/><span className="pink">Minimize</span> costs</div>
         <div className="main2">Empowering your decentralized dreams with expert<br /> audits and Web3 Security.</div>
         <div className="main3">P2PAudit helps Web3 projects meet <span className="pink2"> world leading<br /> independent security experts </span></div>
-        <div className="main3mobile">P2PAudit helps Web3 projects meet <span className="pink2"> world leading independent security experts </span></div>
+        <div className="main3mobile">P2PAudit helps Web3 projects meet <span className="pink2"><br /> world leading independent security<br /> experts </span></div>
         <div className='coinmobile'>
           <img src={coin} alt="coin" className='coinimagemobile' />
         </div>
-        <div className="main2mobile">Empowering your decentralized dreams with expert<br /> audits and Web3 Security.</div>
+        <div className="main2mobile">Empowering your decentralized<br /> dreams with expert audits and Web3<br />  Security.</div>
         <div className="main4">
           <Link onClick={() => { window.scrollTo({ top: 0, left: 0, behavior: 'smooth' }); }} to="/auditorform" className="applyaudit">Apply as an Auditor</Link>
           <Link onClick={() => { window.scrollTo({ top: 0, left: 0, behavior: 'smooth' }); }} to="/requestform" className="requestaudit"><div className="requestauditbtn">Request an audit</div></Link>
