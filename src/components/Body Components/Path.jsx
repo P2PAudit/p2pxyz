c4159ca9fe9e709601f97cf14873b4cfee920099
PRASANT GUPTA <prasant123gupta@gmail.com>
2023-04-27 10:08:32 +0530

    path mobile update

--- src/components/Body Components/Path.jsx
+++ src/components/Body Components/Path.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import path from '../../images/path.svg'
+import pathmobile from '../../images/pathmobile.svg'
 import { Link } from "react-router-dom"
 
 const Path = () => {
   return (
     <div className='pathmain'>
-      <img src={path} alt="path" className='path'/>
+      <img src={path} alt="path" className='path hiddenmobile'/>
+      <img src={pathmobile} alt="path" className='path hidden visiblemobile'/>
       <Link onClick={() => { window.scrollTo({ top: 0, left: 0, behavior: 'smooth' }); }} to="/requestform" className="pathbtn"><div className="requestauditbtn">Request an audit</div></Link>
     </div>
   )
