47dbce37578a015ffa66d34f0cb2ef3286db7dd9
PRASANT GUPTA <prasant123gupta@gmail.com>
2023-05-24 17:16:19 +0530

    report route added and fetching data of reports url

---
+++ src/components/Reports/Reportbox.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Reportbox = () => {
+  return (
+    <div>Reportbox</div>
+  )
+}
+
+export default Reportbox
--- src/components/Reports/Reports.jsx
+++ src/components/Reports/Reports.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import axios from 'axios';
+import { useState ,useEffect } from 'react';
+import Reportbox from './Reportbox';
+const urlserver = "https://p2pauditserver.vercel.app"
 
 const Reports = () => {
+
+  const [reportlineup, setReportlineup] = useState([]);
+
+  useEffect(() => {
+    axios.get(`${urlserver}/getreports`)
+    .then(response => {
+      const data = response.data;
+      setReportlineup(data);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  }, [])
+
+
   return (
-    <div>Reports</div>
+    <div className='reports'>
+      <div className="reporthead">Our Reports</div>
+      <div className="reportssection">
+        {reportlineup.length === 0 ? (
+          <div>Loading.....</div>
+        ) :
+          reportlineup.map((report) => {
+            return (<Reportbox report={report} />)
+          })
+        }
+      </div>
+    </div>
   )
 }
 
